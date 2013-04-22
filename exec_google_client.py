0c733bb2d8399436a8d72abdbd49a73f0f2754ea
Christopher Clearfield <clearf@phad.dreamhost.com>
2013-04-21 18:48:58 -0700

    production shuffle

---
+++ exec_google_client.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import sys, os
+
+activate_this = os.path.join(os.environ['HOME'], 'cal-tracker', 'prod', 'bin', 'activate_this.py') 
+execfile(activate_this, dict(__file__=activate_this)) 
+
+from app.google.google_client import main
+
+if __name__ == '__main__':
+  sys.exit(main())
