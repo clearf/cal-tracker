15e529f8b047cffbbfe658caca9d76ca965a7e53
Clearfield Christopher <chris.clearfield@gmail.com>
2013-05-05 14:48:39 -0400

    Deploying the "squawk" webapp (a wrapper around a simple google form)

--- app/core.py
+++ app/core.py
@@ -30,56 +30,11 @@ def index():
     # Note: to reset the form in JS, do: $('#newrequest').get(0).reset();
     # XXX Consider: Templates are very general, and content is queried from google docs?
     # Or, some templates are specific with flashy content, and only things like blogs come from google
-    return redirect(url_for('overview'))
+    return redirect(url_for('squawks'))
 
-@app.route('/overview/')
-def overview():
-    return renderme('overview')
-
-@app.route('/services/')
-def services():
-    return renderme('services')
-
-@app.route('/team/')
-def team():
-    file_id='1XvQhNs6WcLrogwNDixPeVoW0p4SX8hKgHYvw0eMiJeo'
-    return renderme('team', file_id)
-
-@app.route('/whitepapers/')
-def whitepapers():
-    return renderme('whitepapers')
-
-@app.route('/contact/')
-def contact():
-    return renderme('contact')
-
-@app.route('/commentary/')
-def commentary():
-    if app.debug: 
-        queried_posts = BlogPost.query.order_by(*post_sort_order).all()
-    else:
-        queried_posts = BlogPost.query.filter_by(status='published').order_by(*post_sort_order).all()
-    headline_post = queried_posts[0]
-    # XXX include a pager here
-    other_posts = queried_posts[1:4]
-    return render_template('commentary.html', page='/commentary/', headline_post=headline_post, other_posts=other_posts) 
-
-@app.route('/commentary/posts/<string:title_url>')
-def post_page(title_url):
-    unique_title=urllib.unquote(title_url)
-    post = BlogPost.query.filter_by(unique_title=unique_title).first_or_404()
-    prev_post, next_post=post.get_adjacent_posts()
-    return render_template('post.html', page='/commentary/', post=post, prev_post=prev_post, next_post=next_post)
-
-@app.route('/pieces/posts')
-def get_posts():
-    if app.debug: 
-        queried_posts = BlogPost.query.order_by(*post_sort_order).all()
-    else:
-        queried_posts = BlogPost.query.filter_by(status='published').order_by(*post_sort_order).all()
-    for post in queried_posts:
-        processed_posts.append(post.preview())
-    return Response(json.dumps(processed_posts), mimetype='application/json')
+@app.route('/squawks/')
+def squawks():
+    return render_template('squawks.html')
 
 ##### Main
 def main(args=None, parser=None):
@@ -98,7 +53,7 @@ def main(args=None, parser=None):
     opts = parser.parse_args(args)
     if opts.debug:
         app.debug = True
-    app.run()
+    app.run(port=5001)
     return 0
 
 app.secret_key = '>O8#P+RxSI&opa0HsDIk'
